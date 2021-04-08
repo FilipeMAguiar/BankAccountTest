8daac5b01e15afe4b49115bbf48e3e677ac3789d
Filipe Aguiar <Filipe Aguiar>
2021-04-08 20:16:20 -0300

    first commit

---
+++ src/main/java/south/bank/test/SouthSystemTestApplication.java
@@ -0,0 +1,13 @@
+package south.bank.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthSystemTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SouthSystemTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/south/bank/test/SouthSystemTestApplicationTests.java
@@ -0,0 +1,13 @@
+package south.bank.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthSystemTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
