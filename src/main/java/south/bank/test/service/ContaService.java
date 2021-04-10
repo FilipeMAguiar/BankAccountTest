0b8cc2aa6e47d1945f8f5fa3eca0e3ce4afd0dd1
Filipe Aguiar <Filipe Aguiar>
2021-04-10 16:32:09 -0300

    01 Tentativa de refatoração

--- src/main/java/south/bank/test/service/ContaService.java
+++ src/main/java/south/bank/test/service/ContaService.java
@@ -1,6 +1,9 @@
 package south.bank.test.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import south.bank.test.entity.Conta;
 import south.bank.test.exception.BusinessException;
@@ -17,10 +20,13 @@ public class ContaService {
     private static final String NOT_FOUND_MESSAGE_CONTA = "Não existe nenhuma conta cadastrada";
 
     private final ContaRepository contaRepository;
+    private final MongoTemplate mongoTemplate;
 
     public List<Conta> listarContas(Long id) throws BusinessException {
+        Query query = new Query();
         if (Objects.isNull(id)){
-            List<Conta> contas = this.contaRepository.findAll();
+            query.addCriteria(Criteria.where("conta").is(Conta.class));
+            List<Conta> contas = mongoTemplate.find(query, Conta.class);
             if (contas.isEmpty()){
                 throw new BusinessException(NOT_FOUND_MESSAGE_CONTA);
             }
